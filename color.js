153baee45a9bd6fa964ea6a0b743aebd77bc31e9
VladislavsDz <49658792+VladislavsDz@users.noreply.github.com>
2020-03-22 20:52:17 +0000

    Fix code
    last code wasnt work so change the code

--- color.js
+++ color.js
@@ -1,3 +1,5 @@
-function color() {
-    document.getElementById("changecolor").style.color = "red";
+const elems = document.getElementsByClassName('color') // returns array of elements.
+
+for (let i = 0; i < elems.length; i++) {
+    elems[i].style.backgroundColor = 'red' // loops through the array and assigns the background color to each element.
 }
