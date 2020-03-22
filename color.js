6f7ab727ac6f76770b5442c2db37123136003365
VladislavsDz <49658792+VladislavsDz@users.noreply.github.com>
2020-03-22 20:55:01 +0000

    color.js change code
    1 more time change the code

--- color.js
+++ color.js
@@ -1,5 +1,6 @@
-const elems = document.getElementsByClassName('color') // returns array of elements.
-
-for (let i = 0; i < elems.length; i++) {
-    elems[i].style.backgroundColor = 'red' // loops through the array and assigns the background color to each element.
-}
+function color() {
+var elements = document.getElementsByClassName("changecolor");
+  for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color = "red";
+    }
+};
